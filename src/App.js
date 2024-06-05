b54a1adeeccaffe67b6fb92767968b902243ff0d
Christian Quebral <christianquebral@gmail.com>
2024-06-04 22:35:45 -0700

    add input to parent

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
 
 import Message from './Components/Message';
-import Input from './Components/Input'
+//import Input from './Components/Input'
 
+function Input() {
+  return (
+      <form class="input" action="" method="post" onKeyDown={OnKeyDown}>
+        <span contenteditable>
+          <textarea type="text" id="inputValue" required></textarea>
+        </span>
+      </form>
+  );
+}
+
+function OnKeyDown(e) {
+  if(e.keyCode == 13 && e.shiftKey == false) {
+      var input = document.getElementById("inputValue");
+      // alert(input.value);
+      input.value = "";
+  }
+}
 
 function App() {
   return (
--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,11 +1,19 @@
-function Input() {
-	return (
-	    <form class="input" action="" method="post">
-	      <span contenteditable>
-	        <textarea type="text" name="user_input" required></textarea>
-	      </span>
-	    </form>
-	);
-}
+// function Input() {
+// 	return (
+// 	    <form class="input" action="" method="post" onKeyDown={OnKeyDown}>
+// 	      <span contenteditable>
+// 	        <textarea type="text" id="inputValue" required></textarea>
+// 	      </span>
+// 	    </form>
+// 	);
+// }
 
-export default Input;
+// function OnKeyDown(e) {
+// 	if(e.keyCode == 13 && e.shiftKey == false) {
+//     	var input = document.getElementById("inputValue");
+//     	// alert(input.value);
+//     	input.value = "";
+// 	}
+// }
+
+// export default Input;
