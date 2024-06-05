efdfd8c1966b1ea63179bdea92c91d3bda8377ca
Christian Quebral <christianquebral@gmail.com>
2024-06-04 23:12:28 -0700

    remove components for debugging

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 
-import Message from './Components/Message';
-//import Input from './Components/Input'
+
+var messages = [
+  '12:00:00t00:00:00 | [Aegey] My name is Aegey',
+  '12:00:00t00:00:00 | [Ouillo] And I am Ouillo aka Racecar',
+  '12:00:00t00:00:00 | [Buntonomer] I am Buntonomer, the most gracious being in existence',
+  '12:00:00t00:00:00 | [Difo] I\'m Difo, I\'m going to kill Chridgn',
+]
+
+var messageMap = messages.map(message => <li>{message}</li>)
 
 function Input() {
   return (
-      <form class="input" action="" method="post" onKeyDown={OnKeyDown}>
+      <form class="input" action="" method="post" onKeyDown={OnKeyDown.bind(this)}>
         <span contenteditable>
           <textarea type="text" id="inputValue" required></textarea>
         </span>
@@ -14,9 +21,9 @@ function Input() {
 }
 
 function OnKeyDown(e) {
-  if(e.keyCode == 13 && e.shiftKey == false) {
+  if(e.keyCode === 13 && e.shiftKey === false) {
       var input = document.getElementById("inputValue");
-      // alert(input.value);
+      messages.push(input.value);
       input.value = "";
   }
 }
@@ -24,7 +31,10 @@ function OnKeyDown(e) {
 function App() {
   return (
     <>
-      <div class="message"><Message /></div>
+      <div class="message">
+        <p class="message">{messageMap}
+        </p>
+      </div>
       <div class="input"><Input /></div>
     </>
   )
