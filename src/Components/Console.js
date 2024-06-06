b6fb2f37a897f195db86a183af304bf879f835d8
Christian Quebral <christianquebral@gmail.com>
2024-06-06 11:08:11 -0700

    add logic for session create/join input handling

--- src/Components/Console.js
+++ src/Components/Console.js
@@ -3,7 +3,18 @@ import { useState } from 'react';
 
 function Console() {
   // initialize the main message queue
-  const [messages, setMessages] = useState([]);
+  var systemMessage = [
+      "Welcome to msglite v0.0.0!",
+      "Type commands into the box below.",
+      "Create a session: 'create session={desired session name} name={desired name}'",
+      "Join a session: 'join session={session name} name={desired name}'",
+      "...",
+      "Example: 'create session=my-session name=Hubert'"
+  ]
+  const [messages, setMessages] = useState(systemMessage);
+  var sessionActive = false;
+  var sessionName = null;
+  var userName = null;
 
   const pushMessage = (messageData) => {
     // formatting string elements
@@ -23,7 +34,23 @@ function Console() {
   // pass data back to parent and into the message array
   const inputToConsole = (event, inputData) => {
     if(event.keyCode === 13 && event.shiftKey === false) {
-      pushMessage(inputData);
+      if (sessionActive === true) {
+        pushMessage(inputData);
+      }
+      else {
+        handleCommand(inputData);
+      }
+    }
+  }
+
+  const handleCommand = (inputData) => {
+    // handle create command
+    var inputArray = inputData.split(" ");
+    if (inputArray.length === 3) {
+      pushMessage("Meow");
+    }
+    else {
+      pushMessage("Improperly formatted command! Please see examples above");
     }
   }
 
